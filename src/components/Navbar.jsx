d4c6a8d089f3ba60f1f49e886cbd2967babd6164
qxan <mehmet.enfa@gmail.com>
2023-02-18 18:48:38 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,19 @@ import { Cart, Chat, Notificaion, UserProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
-  <TooltipComponent content={title} position="BottemCenter">
-    <button type="button" onClick={customFunc  }>
-
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      >
+        {icon}
+      </span>
     </button>
   </TooltipComponent>;
 };
