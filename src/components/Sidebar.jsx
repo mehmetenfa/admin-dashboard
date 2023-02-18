93f59dc3ddf5edec59ef3496ba182182bc10f6ad
qxan <mehmet.enfa@gmail.com>
2023-02-18 18:09:18 +0300

    sidebar ok

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,9 +9,9 @@ import { links } from "../data/dummy";
 const Sidebar = () => {
   const activeMenu = true;
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
