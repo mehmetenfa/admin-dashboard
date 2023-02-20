3e9023ca2c9564833d47320b5dd0f982ba0c1f99
qxan <mehmet.enfa@gmail.com>
2023-02-20 17:52:17 +0300

    secondy expense and budget

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -61,14 +61,37 @@ const Ecommerce = () => {
           <div className="flex justify-between">
             <p className="font-semibold text-xl">Revenue Updates</p>
             <div className="flex items-center gap-4">
-                <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                  <span><GoPrimitiveDot /></span>
-                  <span>Expense</span>
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrayp justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
                 </p>
-                <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
-                  <span><GoPrimitiveDot /></span>
-                  <span>Budget</span>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$48,438</span>
                 </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
             </div>
           </div>
         </div>
